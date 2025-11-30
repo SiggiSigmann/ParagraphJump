a9258c4b8e94b6c1b8a0a1f09d5c8a6c66f0c43e
Siggisigmann <siggisigmann@gmail.com>
2025-11-30 09:17:25 +0100

    add base file for the plugin

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("scala")
+    id("org.jetbrains.intellij.platform") version "2.10.5"
+}
+
+group = "com.github.siggisigmann"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+// Configure IntelliJ Platform Gradle Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        create("IC", "2025.2")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+      // Add necessary plugin dependencies for compilation here, example:
+      //bundledPlugin("com.intellij.java")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "242"
+        }
+
+        changeNotes = """
+            Initial version
+        """.trimIndent()
+    }
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+}
